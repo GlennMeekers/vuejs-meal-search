d805ff63c89bca1c8e2abe92376a8fe92e7ce9b9
GlennMeekers <glennmeekers@gmail.com>
2025-03-20 14:50:00 +0100

    tailwindcss fix, interface: letter list and basic header nav, axiosClient, letter route

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,37 @@
 <template>
-  <pre>
-    {{ meals }}
-</pre
-  >
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+    />
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        v-bind:to="{ name: 'byLetter', params: { letter } }"
+        v-bind:key="letter"
+        v-for="letter in letters"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+
+    <pre>{{ ingredients }}</pre>
+  </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from "../store/index.js";
+import axiosClient from "../axiosClient.js";
 
-const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const ingredients = ref([]);
+
+onMounted(() => {
+  axiosClient.get("/list.php?i=list").then((reponse) => {
+    console.log(reponse.data);
+    ingredients.value = reponse.data.meals;
+  });
+});
 </script>
